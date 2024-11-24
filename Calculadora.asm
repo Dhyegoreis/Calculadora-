e05fc7e63b35dab1c0d17b96559172c6568ff2fd
Dhyegoreis <167694509+Dhyegoreis@users.noreply.github.com>
2024-11-24 18:43:27 -0300

    commit

--- Calculadora.asm
+++ Calculadora.asm
@@ -1,46 +1,50 @@
 section .data 
+m0 db 'Bem vindo a Calculadora!!', 0xa 
+tm0 equ $ - m0
+
 m1 db 'Digite o primeiro numero:', 0xa 
 tm1 equ $ - m1
 
 m2 db 'Digite o segundo numero:',0xa
 tm2 equ $ - m2
 
-m3 db 'Qual operação sera efetuada:',0xa
-tm3 equ $ - m3
+    m3 db "Qual operação será efetuada:", 0xA
+         db "1 para somar", 0xA
+         db "2 para subtração", 0xA
+         db "3 para multiplicação", 0xA
+         db "4 para divisão", 0xA
+         db "0 para sair da calculadora", 0
+    tm3 equ $ - m3  
 
-m4 db '1 para somar  ',0xa
+m4 db 'Digite sua escolha:',0xa
 tm4 equ $ - m4
 
-m5 db '2 para subtração.  ',0xa
+m5 db 'O Resutado é :',0xa
 tm5 equ $ - m5
 
-m6 db '3 para multiplicação. ',0xa
+m6 db 'Escolha errada',0xa
 tm6 equ $ - m6
 
-m7 db '4 para divisão ',0xa
-tm7 equ $ - m7
-
-m8 db '0 sair da calculadora ',0xa
-tm8 equ $ - m8
+m7 db 'Deseja fazer outra operação ? (1 - Sim, 0 - Não):',0xa
+tm6 equ $ - m7
 
 section .bss
 
     n1 resb 2
     n2 resb 2 
-    ope4 resb 2
-    ope5 resb 2
-    ope6 resb 2
-    ope7 resb 2
-    ope8 resb 2
-
-
-
-
-
+    n4 resb 2
+    n_e resb 2
+    Resutado resb 2
+   
 section .text 
     global _start 
 
 _start:
+    mov eax, 4
+     mov ebx, 1
+     mov ecx, m0
+     mov edx,  tm0
+     int 0x80
 
      mov eax, 4
      mov ebx, 1
@@ -66,7 +70,7 @@ _start:
      mov edx,  2
      int 0x80
      
-         mov eax, 4
+     mov eax, 4
      mov ebx, 1
      mov ecx, m3
      mov edx,  tm3
@@ -75,85 +79,38 @@ _start:
      mov eax, 4
      mov ebx, 1
      mov ecx, m4
-     mov edx,  tm4
+     mov edx, tm4
      int 0x80
 
-      mov eax, 4
-     mov ebx, 1
-     mov ecx, m5
-     mov edx,  tm5
+     mov eax, 3
+     mov ebx, 0
+     mov ecx, n4
+     mov edx,  2
      int 0x80
 
+     movzx eax, byte [n4]
+     sub eax, '0'
+
+    ; Verifica se a escolha é válida
+     cmp eax, 0
+     jl n_e
+     cmp eax, 4
+     jg n_e
+
+     cmp eax, 0                 
+     je exit
+
+     call m4
+     jmp m7
+
+escolha_invalida:
+    ; Mensagem de erro
      mov eax, 4
      mov ebx, 1
      mov ecx, m6
-     mov edx,  tm6
+     mov edx, tm6
      int 0x80
-
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, m7
-     mov edx,  tm7
-     int 0x80
-
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, m8
-     mov edx,  tm8
-     int 0x80
-     
-     mov al, [n1]
-     mov bl, [n2]
-     sub al, '0'
-     sub bl, '0'
-     add al,bl
-     add al, '0'
-     mov [resposta], al
-     int 0x80
-
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, resposta
-     mov edx, 2
-      int 0x80
-    
-     mov al, [n1]
-     mov bl, [n2]
-     sub al, '0'
-     sub bl, '0'
-     mul bl
-     add al, '0'
-     mov [resposta], al
-      int 0x80
-
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, resposta
-     mov edx, 2
-     int 0x80
-    
-     mov al, [n1]
-     mov bl, [n2]
-     sub al, '0'
-     sub bl, '0'
-     div bl
-     add al, '0'
-     add ah, '0'
-     mov [resposta], al
-     mov [resto], ah
-     int 0x80
-
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, resposta
-     mov edx, 2
-     int 0x80
-    
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, resto
-     mov edx, 2
-     int 0x80
+     jmp obter_escolha
 
 sair:
     mov eax, 1
